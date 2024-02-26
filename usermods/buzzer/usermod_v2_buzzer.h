e3c2692f48f9de7c733c804075ae04810eb966e4
Thijs Triemstra <info@collab.nl>
2024-02-26 02:04:24 +0100

    make buzzer work on esp32

--- usermods/buzzer/usermod_v2_buzzer.h
+++ usermods/buzzer/usermod_v2_buzzer.h
@@ -7,13 +7,17 @@
 
 #define USERMOD_ID_BUZZER 900
 #ifndef USERMOD_BUZZER_PIN
-#define USERMOD_BUZZER_PIN GPIO_NUM_32
+#define USERMOD_BUZZER_PIN GPIO_NUM_13
 #endif
+
+// The ESP32 has 16 channels which can generate 16 independent waveforms
+// We'll just choose PWM channel 0 here
+const int TONE_PWM_CHANNEL = 0;
 
 /*
  * Usermods allow you to add own functionality to WLED more easily
  * See: https://github.com/Aircoookie/WLED/wiki/Add-own-functionality
- * 
+ *
  * Using a usermod:
  * 1. Copy the usermod into the sketch folder (same folder as wled00.ino)
  * 2. Register the usermod by adding #include "usermod_filename.h" in the top and registerUsermod(new MyUsermodClass()) in the bottom of usermods_list.cpp
@@ -21,54 +25,92 @@
 
 class BuzzerUsermod : public Usermod {
   private:
+    bool enabled = false;
     unsigned long lastTime_ = 0;
     unsigned long delay_ = 0;
-    std::deque<std::pair<uint8_t, unsigned long>> sequence_ {};
+    bool _pinAttached = false;
+    std::deque<std::pair<note_t, unsigned long>> sequence_ {};
   public:
-    /*
-     * setup() is called once at boot. WiFi is not yet connected at this point.
-     * You can use it to initialize variables, sensors or similar.
+
+    /**
+     * Enable/Disable the usermod
      */
+    inline void enable(bool enable) { enabled = enable; }
+
+    /**
+     * Get usermod enabled/disabled state
+     */
+    inline bool isEnabled() { return enabled; }
+
     void setup() {
-      // Setup the pin, and default to LOW
-      pinMode(USERMOD_BUZZER_PIN, OUTPUT);
-      digitalWrite(USERMOD_BUZZER_PIN, LOW);
-
       // Beep on startup
-      sequence_.push_back({ HIGH, 50 });
-      sequence_.push_back({ LOW, 0 });
+      sequence_.push_back({ note_t::NOTE_D, 100 });
+      sequence_.push_back({ note_t::NOTE_Eb, 100 });
     }
-
 
     /*
      * connected() is called every time the WiFi is (re)connected
      * Use it to initialize network interfaces
      */
     void connected() {
-      // Double beep on WiFi
-      sequence_.push_back({ LOW, 100 });
-      sequence_.push_back({ HIGH, 50 });
-      sequence_.push_back({ LOW, 30 });
-      sequence_.push_back({ HIGH, 50 });
-      sequence_.push_back({ LOW, 0 });
+      // beep on WiFi
+      sequence_.push_back({ note_t::NOTE_A, 150 });
+      sequence_.push_back({ note_t::NOTE_D, 80 });
+    }
+
+    /**
+     * handleButton() can be used to override default button behaviour. Returning true
+     * will prevent button working in a default way.
+     */
+    bool handleButton(uint8_t b) {
+      yield();
+      // ignore certain button types as they may have other consequences
+      if (!enabled
+       || buttonType[b] == BTN_TYPE_NONE
+       || buttonType[b] == BTN_TYPE_RESERVED
+       || buttonType[b] == BTN_TYPE_PIR_SENSOR
+       || buttonType[b] == BTN_TYPE_ANALOG
+       || buttonType[b] == BTN_TYPE_ANALOG_INVERTED) {
+        return false;
+      }
+
+      bool handled = false;
+
+      // do your button handling here
+      //sequence_.push_back({ note_t::NOTE_G, 50 });
+
+      return handled;
     }
 
     /*
      * loop() is called continuously. Here you can check for events, read sensors, etc.
      */
     void loop() {
+      if (millis() - lastTime_ <= delay_) {
+        return; // Wait until delay has elapsed
+      } else {
+        if (_pinAttached) {
+          // turn off tone, write out a PWM waveform with 0 frequency
+          ledcDetachPin(USERMOD_BUZZER_PIN);
+          _pinAttached = false;
+        }
+      }
       if (sequence_.size() < 1) return; // Wait until there is a sequence
-      if (millis() - lastTime_ <= delay_) return; // Wait until delay has elapsed
 
       auto event = sequence_.front();
       sequence_.pop_front();
 
-      digitalWrite(USERMOD_BUZZER_PIN, event.first);
+      if (!_pinAttached) {
+        ledcAttachPin(USERMOD_BUZZER_PIN, TONE_PWM_CHANNEL);
+        _pinAttached = true;
+      }
+      uint8_t octave = 4;
+      ledcWriteNote(TONE_PWM_CHANNEL, event.first, octave);
+
       delay_ = event.second;
 
       lastTime_ = millis();
     }
-
 
     /*
      * getId() allows you to optionally give your V2 usermod an unique ID (please define it in const.h!).
